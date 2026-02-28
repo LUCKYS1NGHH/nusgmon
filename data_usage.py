138d4eb2cdca4efce45ffbf6ddf7d1d3f715e85e
LUCKY <luckysingh71826@gmail.com>
2026-02-28 14:48:42 +0530

    Convert log loop to function called log_net_usage()

--- data_usage.py
+++ data_usage.py
@@ -19,30 +19,30 @@ old = psutil.net_io_counters()
 def to_mb(_bytes):
     return _bytes / 1024 ** 2
 
-while True:
+def log_net_usage():
+    while True:
+        print("\nWait 3 seconds...\n")
+        time.sleep(3)
 
-    print("\nWait 3 seconds...\n")
-    time.sleep(3)
+        new = psutil.net_io_counters()
 
-    new = psutil.net_io_counters()
+        # speed calculate
+        download = new.bytes_recv - old.bytes_recv
+        upload = new.bytes_sent - old.bytes_sent
 
-    # speed calculate
-    download = new.bytes_recv - old.bytes_recv
-    upload = new.bytes_sent - old.bytes_sent
+        print("-" * 40)
 
-    print("-" * 40)
+        print(f"Total Megabytes Received : {round(to_mb(new.bytes_recv))} MB")
+        print(f"Total Megabytes Sent     : {round(to_mb(new.bytes_sent))} MB")
 
-    print(f"Total Megabytes Received : {round(to_mb(new.bytes_recv))} MB")
-    print(f"Total Megabytes Sent     : {round(to_mb(new.bytes_sent))} MB")
+        print(f"Download                 : {download / 1024:.2f} KB/s")
+        print(f"Upload                   : {upload / 1024:.2f} KB/s")
 
-    print(f"Download                 : {download / 1024:.2f} KB/s")
-    print(f"Upload                   : {upload / 1024:.2f} KB/s")
+        print("-" * 40)
 
-    print("-" * 40)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cur.execute("INSERT INTO data_usage (timestamp, bytes_sent, bytes_recv) VALUES (?, ?, ?)",
+            (int(time.time()), upload, download))
+        conn.commit()
 
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    cur.execute("INSERT INTO data_usage (timestamp, bytes_sent, bytes_recv) VALUES (?, ?, ?)",
-        (int(time.time()), upload, download))
-    conn.commit()
-
-    old = new
+        old = new
