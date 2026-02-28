3bde7f172f1f48aa5d90d840877943156570f036
LUCKY <luckysingh71826@gmail.com>
2026-02-28 15:51:58 +0530

    Add -v for verbose and -V for version

--- data_usage.py
+++ data_usage.py
@@ -19,14 +19,15 @@ CREATE TABLE IF NOT EXISTS data_usage (
 def to_mb(_bytes):
     return _bytes / 1024 ** 2
 
-def log_net_usage(wait=None, dry_run=False):
+def log_net_usage(wait=None, dry_run=False, verbose=False):
     wait = 3 if wait is None else wait
 
     old = psutil.net_io_counters()
     count = 0
 
     while True:
-        print(f"\nWait {wait} seconds...\n")
+        if verbose:
+            print(f"\nWait {wait} seconds...\n")
         time.sleep(wait)
 
         new = psutil.net_io_counters()
@@ -35,15 +36,13 @@ def log_net_usage(wait=None, dry_run=False):
         download = new.bytes_recv - old.bytes_recv
         upload = new.bytes_sent - old.bytes_sent
 
-        print("-" * 40)
-
-        print(f"Total Megabytes Received : {round(to_mb(new.bytes_recv))} MB")
-        print(f"Total Megabytes Sent     : {round(to_mb(new.bytes_sent))} MB")
-
-        print(f"Download                 : {download / 1024:.2f} KB/s")
-        print(f"Upload                   : {upload / 1024:.2f} KB/s")
-
-        print("-" * 40)
+        if verbose:
+            print("-" * 37)
+            print(f"Total Megabytes Received : {round(to_mb(new.bytes_recv))} MB")
+            print(f"Total Megabytes Sent     : {round(to_mb(new.bytes_sent))} MB")
+            print(f"Download                 : {(download / wait) / 1024:.2f} KB/s")
+            print(f"Upload                   : {(upload / wait) / 1024:.2f} KB/s")
+            print("-" * 37)
 
         # only log when not dry-run and download/upload not 0
         if not dry_run and (download or upload):
@@ -64,11 +63,13 @@ record_parser = subparsers.add_parser("record")
 
 record_parser.add_argument("-w", "--wait", type=float, help="record after certain seconds (default: 3)")
 record_parser.add_argument("-d", "--dry-run", action="store_true", help="prevent logging to database")
+record_parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose output")
+parser.add_argument("-V", "--version", action="version", version="1.0.0")
 args = parser.parse_args()
 
 if args.command == "record":
     try:
-        log_net_usage(args.wait, args.dry_run)
+        log_net_usage(args.wait, args.dry_run, args.verbose)
     except KeyboardInterrupt:
         conn.commit()
         conn.close()
